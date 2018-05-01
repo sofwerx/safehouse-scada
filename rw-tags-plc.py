da8c3ac91772b1c52b737d6e9f58c0118cca6351
sw901 <swheeler1@mail.usf.edu>
2018-05-01 13:56:09 -0400

    Session Control Bug Fixes

--- rw-tags-plc.py
+++ rw-tags-plc.py
@@ -8,80 +8,41 @@ from threading import Timer
 from time import sleep
 
 
-
-def motorFunc(t_end):
+def motor_state(t_end):
     print'motor on'
     while time.time() < t_end:
         c.write_tag([('nerfgun_position_fire',0,'BOOL'),('trigger_motor_on',1,'BOOL')])
-        #c.write_tag(('trigger_bullet_fire',1,'BOOL'))
     c.write_tag(('trigger_bullet_fire',1,'BOOL'))
 
-def fireBullets(t_end2):
-    #c.write_tag([('trigger_bullet_fire',1,'BOOL')])
 
+def fire_bullets(t_end2):
     print'Fire Bullets'
     while time.time() < t_end2:
         c.write_tag([('trigger_bullet_fire',1,'BOOL')])
     c.write_tag([('trigger_bullet_fire',0,'BOOL'),('trigger_motor_on',0,'BOOL')])
 
 
-def hideGun(t_end3):
+def hide_gun():
     print 'Hide Gun'
     time.sleep(1)
     c.write_tag(('nerfgun_position_fire',1,'BOOL'))
 
-def userInput(arg):
-    if arg is 'quit':
-        exit(0)
-        sleep(1)
 
-def parseMessage(message,fp):
+def parse_msg(message,fp):
     t_end = time.time() + 60 * .02
     t_end2 = time.time() + 60 * .058
-    t_end3 = time.time() + 60 * .16
 
-    if "longgun" in message: 
-        motorFunc(t_end)
-        fireBullets(t_end2)
-        hideGun(t_end3)    
+    if "longgun" in message:
+        motor_state(t_end)
+        fire_bullets(t_end2)
+        hide_gun()
         while "longgun" in message:
             message = fp.readline()
 
-#sections = message.split(";")
-    #Quit = input('Press Q to Quit')
 
-    #for section in sections:
-      #  pair = section.split('=')
-
-       # if(len(pair) == 2):
-           # print(pair[0] + " " + pair[1])
-        #   if pair[0] is 'obj'
-	#	print pair[1] 
-	#	if pair[1] is 'longgun'
-            
-	 #       motorFunc()
-          #      fireBullets()
-           #     hideGun()
-            #    continue
-        #elif Quit == 'Q': 
-         #   print 'User quit program'
-         #   c.write_tag([('nerfgun_position_fire',1,'BOOL'),('trigger_motor_on',0,'BOOL')])
-         #  c.write_tag([('trigger_bullet_fire',0,'BOOL')])
-         # break
-        #else:
-         #   continue
-    #sys.exit(0)
-
-#def handleCh(ch):
-#    if(ch == '\n'):
-#        parseMessage(sample_message)
-#        sample_message = ""
-#    else:
-#        sample_message += ch
-
-def handleInput(incomingMessage):
-    for ch in incomingMessage:
-        handleCh(ch)
+# def handleInput(incomingMessage):
+#     for ch in incomingMessage:
+#         handleCh(ch)
 
 if __name__ == '__main__':
 
@@ -90,28 +51,23 @@ if __name__ == '__main__':
     #     format="%(levelname)-10s %(asctime)s %(message)s",
     #     level=logging.DEBUG
     # )
+
     c = ClxDriver()
 
     print c['port']
     print c.__version__
 
-
     filepath = "/home/nvidia/Documents/yolov2/capturedetect"
     if c.open('192.168.0.203'):
 
-        with open(filepath) as fp:  
+        with open(filepath) as fp:
             while 1:
-		line = fp.readline()
+                line = fp.readline()
                 try:
-                    parseMessage(line,fp)
-                   # fp.close()
-                except Exception as e:
+                    parse_msg(line,fp)
+                except ValueError as e:
                     c.close()
                     print e
                     pass
 
-        # r_array = c.read_array("TotalCount", 1750)
-        # for tag in r_array:
-        #     print (tag)
-
-        c.close()
+    c.close()
